902a11002c4f8befd167fb662c0eeaeb2d8769cd
AndreaPieLab <andrea.aschauer@fh-hagenberg.at>
2017-07-10 17:58:30 +0200

    low poly model, render texture

---
+++ Assets/Scripts/Projection/RenderCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderCamera : MonoBehaviour {
+
+    static public Texture2D renderedTexture;
+    public Material mat;
+
+	void Awake () {
+        renderedTexture = new Texture2D(Screen.width, Screen.height);
+        mat.mainTexture = renderedTexture;
+	}
+	
+	void OnPostRender () {
+        renderedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        renderedTexture.Apply();
+	}
+}
