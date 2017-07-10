89b5d4a69bc9402836152aee9251dc7952b25d22
AndreaPieLab <andrea.aschauer@fh-hagenberg.at>
2017-07-10 11:30:06 +0200

    Basic Setup Deep Space Simulator

---
+++ Assets/Scripts/VR/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("displays connected: " + Display.displays.Length);
+
+        for (int i = 0; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
+/*        Display.displays[0].Activate();
+        if (Display.displays.Length > 1)
+            Display.displays[1].Activate();
+        if (Display.displays.Length > 2)
+            Display.displays[2].Activate();*/
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
