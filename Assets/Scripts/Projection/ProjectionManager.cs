8eafc9c44316c8c12595d5000eaee03f24231311
Pie <playfulinteractiveenvironments@gmail.com>
2017-07-17 16:45:17 +0200

    .

---
+++ Assets/Scripts/Projection/ProjectionManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectionManager : MonoBehaviour {
+
+    public GameObject wallProjectionPlane;
+    public GameObject floorProjectionPlane;
+    public Material wallProjectionCamera;
+    public Material floorProjectionCamera;
+    public bool duplicateWallProjection;
+
+	// Use this for initialization
+	void Start () {
+        wallProjectionPlane.GetComponent<Renderer>().material = wallProjectionCamera;
+
+        if (!duplicateWallProjection)
+            floorProjectionPlane.GetComponent<Renderer>().material = floorProjectionCamera;
+        else
+            floorProjectionPlane.GetComponent<Renderer>().material = wallProjectionCamera;
+    }
+}
