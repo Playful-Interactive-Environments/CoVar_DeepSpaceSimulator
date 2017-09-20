cdf76f7fa14f996a87617a943aed704d1d7c3c76
Pie <playfulinteractiveenvironments@gmail.com>
2017-09-20 18:43:27 +0200

    teleport to gallery, load scene menu

---
+++ Assets/Examples/ShallowTime/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : AManager<GameManager>
+{
+    public Text Text1;
+    public Text Text2;
+    public Text RedPoints;
+    public Text BluePoints;
+    public Text GreenPoints;
+    public Text YellowPoints;
+    public GameObject Floor;
+    public Camera MainCam;
+    public GameObject TrackingManager;
+    public GameObject GarbagePrefab;
+    public float Mass;
+    public float Damping;
+    public float Stiftness;
+    public float LineMax;
+    public float LineLow;
+    public float WidthMult;
+    public float AddedForce;
+    public float ForceHeight;
+    public int RedPoint;
+    public int BluePoint;
+    public int YellowPoint;
+    public int GreenPoint;
+
+    void Start () {
+		MainCam = Camera.main;
+    }
+	
+	void Update ()
+	{
+	    RedPoints.text = "" + RedPoint;
+        BluePoints.text = "" + BluePoint;
+        GreenPoints.text = "" + GreenPoint;
+        YellowPoints.text = "" + YellowPoint;
+
+    }
+}
---
+++ Assets/Examples/ShallowTime/Assets/Scripts/Garbage.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Garbage : MonoBehaviour
+{
+    public TestPlayer Parent;
+    public float MoveSpeed = .1f;
+    private float Radius = .1f;
+    public bool released;
+
+    private float stiftness = 1f;
+    private float damping = .5f;
+    private float mass = .5f;
+    private Vector3 velocity;
+    public Rigidbody rigidbody;
+    public GameObject Particles;
+    public int GarbageType;
+    public Material[] Mats;
+    private bool pointCounted;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = true;
+        rigidbody.useGravity = false;
+        GarbageType = Mathf.RoundToInt(Random.Range(0, 3.5f));
+        if (GarbageType == 0)
+            GetComponentInChildren<Renderer>().material = Mats[0];
+        if (GarbageType == 1)
+            GetComponentInChildren<Renderer>().material = Mats[1];
+        if (GarbageType == 2)
+            GetComponentInChildren<Renderer>().material = Mats[2];
+        if (GarbageType == 3)
+            GetComponentInChildren<Renderer>().material = Mats[3];
+    }
+
+    void Update()
+    {
+        stiftness = GameManager.Instance.Stiftness;
+        damping = GameManager.Instance.Damping;
+        mass = GameManager.Instance.Mass;
+        if (!released && Parent != null)
+        {
+            Vector3 stretch = transform.position - Parent.transform.position;
+            Vector3 force = -stiftness * stretch - damping * velocity;
+            Vector3 acceleration = force / mass;
+            velocity += acceleration * Time.deltaTime;
+            transform.position += velocity * Time.deltaTime;
+        }
+        if (released && rigidbody.isKinematic)
+        {
+            transform.position += velocity * Time.deltaTime;
+        }
+    }
+
+    public void Release()
+    {
+        rigidbody.isKinematic = false;
+        rigidbody.useGravity = true;
+        //rigidbody.AddForce(velocity * GameManager.Instance.AddedForce);
+        rigidbody.AddRelativeForce(new Vector3(velocity.x, velocity.y + GameManager.Instance.ForceHeight, velocity.z) * GameManager.Instance.AddedForce);
+        released = true;
+        Parent.myGarbage = null;
+        Parent = null;
+       // Invoke("DestroyGarbage", 5f);
+    }
+
+    public void Remove()
+    {
+        released = true;
+        Parent.myGarbage = null;
+        Parent = null;
+        Invoke("DestroyGarbage", 5f);
+    }
+    void DestroyGarbage()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("DropArea"))
+        {
+            Release();
+        }
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.CompareTag("GarbageBin") && !pointCounted)
+        {
+            if (other.transform.name == "binBlue" && GarbageType == 0)
+            {
+                GameManager.Instance.BluePoint += 1;
+                pointCounted = true;
+                Instantiate(Particles, transform.position, Quaternion.identity);
+            }
+            if (other.transform.name == "binRed" && GarbageType == 1)
+            {
+                GameManager.Instance.RedPoint += 1;
+                pointCounted = true;
+                Instantiate(Particles, transform.position, Quaternion.identity);
+            }
+            if (other.transform.name == "binGreen" && GarbageType == 2)
+            {
+                GameManager.Instance.GreenPoint += 1;
+                pointCounted = true;
+                Instantiate(Particles, transform.position, Quaternion.identity);
+            }
+            if (other.transform.name == "binYellow" && GarbageType == 3)
+            {
+                GameManager.Instance.YellowPoint += 1;
+                pointCounted = true;
+                Instantiate(Particles, transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
+
+/*
+ CODE GRAVEYARD
+     //private float restingLength = .5f;
+    //private float springConstant = .1f;
+    //private float dampingValue = .1f;
+    //private float parentVelocity;
+
+             //rotate around object
+            //_angle += MoveSpeed * Time.deltaTime;
+            //_centre = Parent.transform.position;
+            //Vector3 offset = new Vector3(Mathf.Sin(_angle), 0, Mathf.Cos(_angle)) * Radius;
+            //transform.position = _centre + offset;
+
+
+                 //float displacedLength = Vector3.Distance(transform.position, Parent.transform.position) - restingLength;
+            //Vector3 displacement = (transform.position - Parent.transform.position).normalized;
+            //transform.position += -springConstant * displacedLength * displacement;
+     */
